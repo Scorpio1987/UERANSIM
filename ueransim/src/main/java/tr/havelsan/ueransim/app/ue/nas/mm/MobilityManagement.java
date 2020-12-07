7ce4d04a38bd11bb5d86aa05a5cc333a2c093740
scaltunsoy <scaltunsoy@gmail.com>
2020-12-07 16:18:28 +0300

    changing log tag

--- ueransim/src/main/java/tr/havelsan/ueransim/app/ue/nas/mm/MobilityManagement.java
+++ ueransim/src/main/java/tr/havelsan/ueransim/app/ue/nas/mm/MobilityManagement.java
@@ -65,7 +65,7 @@ public class MobilityManagement {
         } else if (message instanceof DlNasTransport) {
             SessionManagement.receiveDl(ctx, (DlNasTransport) message);
         } else {
-            Log.error(Tag.MSG, "Unhandled message received: %s", message.getClass().getSimpleName());
+            Log.error(Tag.PROC, "Unhandled message received: %s", message.getClass().getSimpleName());
         }
     }
 
