e3e5486b5b715a913370060e2a3c038cafac1dd0
scaltunsoy <scaltunsoy@gmail.com>
2021-01-05 18:01:50 +0300

    ansicolor bugfix

--- ueransim/src/main/java/tr/havelsan/ueransim/app/common/sw/SwCommandResponse.java
+++ ueransim/src/main/java/tr/havelsan/ueransim/app/common/sw/SwCommandResponse.java
@@ -1,14 +1,12 @@
 package tr.havelsan.ueransim.app.common.sw;
 
-import tr.havelsan.ueransim.utils.jcolor.AnsiColorFormat;
-
 public class SwCommandResponse extends SocketWrapper {
     public final int transactionId;
     public final boolean isEnd;
-    public final AnsiColorFormat color;
+    public final String color;
     public final String message;
 
-    public SwCommandResponse(int transactionId, boolean isEnd, AnsiColorFormat color, String message) {
+    public SwCommandResponse(int transactionId, boolean isEnd, String color, String message) {
         this.transactionId = transactionId;
         this.isEnd = isEnd;
         this.color = color;
